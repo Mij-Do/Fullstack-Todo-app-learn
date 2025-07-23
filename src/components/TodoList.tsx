0b82b5da1b04dd8f89da4df02ff8d350c94694c8
ahmed <ahmedmijdo2@gmail.com>
2025-07-23 14:05:27 +0300

    Modal Update To Do

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,11 +1,21 @@
 import Button from "./ui/Button";
 import useAuthenticatedQuery from "../hooks/useAuthenticatedQuery";
+import Modal from "./ui/Modal";
+import Input from "./ui/Input";
+import { useState } from "react";
 
 
 const TodoList = () => {
   const storageKey = "loggedInUser";
   const userDataString = localStorage.getItem(storageKey);
   const userData = userDataString ? JSON.parse(userDataString) : null;
+
+  // states
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onToggleEditModal = () => {
+    setIsOpen(prev => !prev);
+  }
 
   const {isLoading, data} = useAuthenticatedQuery({
     queryKey: ["todos"],
@@ -24,16 +34,24 @@ const TodoList = () => {
     <div key={todo.id} className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
       <p className="w-full font-semibold"> {todo.title} </p>
       <div className="flex items-center justify-end w-full space-x-3">
-        <Button size={'sm'}>Edit</Button>
+        <Button size={'sm'} onClick={onToggleEditModal}>Edit</Button>
         <Button variant={'danger'} size={'sm'}>Remove</Button>
       </div>
     </div>
   )
 
 
+
   return (
     <div className="space-y-1 ">
       {data.todos.length ? renderToDos : <h3>No ToDos yet!!</h3>}
+      <Modal isOpen={isOpen} closeModal={onToggleEditModal} title="Edit ToDo">
+        <Input />
+        <div className="flex items-center justify-around space-x-3 mt-5">
+          <Button>Update</Button>
+          <Button variant={'cancel'} onClick={onToggleEditModal}>Cancel</Button>
+        </div>
+      </Modal>
     </div>
   );
 };
