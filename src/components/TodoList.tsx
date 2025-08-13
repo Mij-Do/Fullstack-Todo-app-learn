ae4e01200578495918b3f6e33d5456452a5d8f53
ahmed <ahmedmijdo2@gmail.com>
2025-08-13 09:50:34 +0300

    fetch data with useQueryHook

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,36 +1,38 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import Button from "../components/ui/Button";
 import axiosInstance from "../config/axios.instance";
 
 const TodoList = () => {
+    // states
     const storageKey = "loggedInUser";
     const userDataString = localStorage.getItem(storageKey);
     const userData = userDataString ? JSON.parse(userDataString) : null;
-    const [todos, setTodos] = useState([]);
-    useEffect(() => {
-        try {
-            axiosInstance.get("/users/me?populate=todos", {
+
+    const {isLoading, data} = useQuery({
+        queryKey: ["todos"],
+        queryFn: async () => {
+            const {data} = await axiosInstance.get("/users/me?populate=todos", {
                 headers: {
                     Authorization: `Bearer ${userData.jwt}`
                 }
-            }).then(res => setTodos(res.data.todos))
-            .catch(err => console.log(err));
-        } catch (error) {
-            console.log(error)
+            });
+            return data;
         }
-    }, [userData.jwt]);
+    }); 
 
+    if (isLoading) return <h3>Loading ...</h3>
+    
     return (
         <div className="space-y-1">
-            {todos.map(todo => (
+            {data.todos.length ? data.todos.map(todo => (
                 <div key={todo.id} className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
-                    <p className="w-full font-semibold">{todo.id} - {todo.title}</p>
+                    <p className="w-full font-semibold">1- {todo.title}</p>
                     <div className="flex items-center justify-end w-full space-x-3">
                         <Button size={"sm"}>Edit</Button>
                         <Button variant={"danger"} size={"sm"}>Remove</Button>
                     </div>
                 </div>
-            ))}
+            )) : <h3> No Todos yet! </h3>}
         </div>
     )
 }
